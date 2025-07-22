05ff46d21158b6fa0e508f878d1418a4190bbe13
SurpassHR <hu.rui0530@gmail.com>
2025-07-22 12:20:26 +0800

    feat: 增加 ArgParser 支持命令行参数驱动脚本

--- run.py
+++ run.py
@@ -1,18 +1,17 @@
-import os
-
 from src.extractor import Extractor
 from src.utils.configLoader import loadConfig
+from src.utils.argParser import ArgParser
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    arg_parser = ArgParser()
+    args = arg_parser.parse_args()
+
     config = loadConfig()
 
-    dataFolder: str = config.get('game_data_dir', '')
-    outputFolder: str = config.get('output_data_dir', '')
+    dataFolder: str = args.dataFolder or config.get("game_data_dir", "")
+    outputFolder: str = args.outputFolder or config.get("output_data_dir", "")
 
-    dataExtractor = Extractor(
-        dataFolder=dataFolder,
-        outputFolder=outputFolder
-    )
+    dataExtractor = Extractor(dataFolder=dataFolder, outputFolder=outputFolder)
     dataExtractor.extract()
 
     # jsFolder = os.path.join(dataFolder, '..', 'js')
---
+++ src/utils/argParser.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+class ArgParser:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            description="Extract data from RPG Maker game."
+        )
+        self._add_arguments()
+
+    def _add_arguments(self):
+        """
+        Add arguments to the parser.
+        """
+        self.parser.add_argument(
+            "--dataFolder", type=str, help="Path to the data folder"
+        )
+        self.parser.add_argument(
+            "--outputFolder", type=str, help="Path to the output folder"
+        )
+
+    def parse_args(self):
+        """
+        Parse command line arguments.
+        """
+        return self.parser.parse_args()
